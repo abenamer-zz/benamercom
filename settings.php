eb86a79a3f8524031bb802a89726eba07c5ef695
Allan Benamer <allan@socialmediatoday.com>
2015-07-11 18:31:10 -0400

    Adding custom settings.php

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+$cookie_domain = '.example.com';
+
+$update_free_access = FALSE;
+
+$local_settings = dirname(__FILE__) . '/settings.local.php';
+if (file_exists($local_settings)) {
+    require_once($local_settings);
