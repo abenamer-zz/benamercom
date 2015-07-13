9e5fd3da290a011e63f1af36a83cba8bfad13b71
Allan Benamer <allan@socialmediatoday.com>
2015-07-12 22:26:26 -0400

    Adding chartbeat timer script

--- themes/benamer/js/script.js
+++ themes/benamer/js/script.js
@@ -12,6 +12,7 @@
 // - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
 (function ($, Drupal, window, document, undefined) {
 
+    var _sf_startpt=(new Date()).getTime()
 
 // To understand behaviors, see https://drupal.org/node/756722#behaviors
 Drupal.behaviors.my_custom_behavior = {
