ba8702ee7912e8ff00b3f6556f161700cd2b5cac
Allan Benamer <allan@socialmediatoday.com>
2015-07-15 16:23:17 -0400

    Making the search field more responsive and nicer to look at

--- themes/benamer/template.php
+++ themes/benamer/template.php
@@ -65,7 +65,7 @@ function benamer_form_alter(&$form, &$form_state, $form_id) {
         $form['search_block_form']['#default_value'] = t('Search Site'); // Set a default value for the textfield
         $form['actions']['submit']['#value'] = t('Search'); // Change the text on the submit button
         $form['actions']['submit']['#attributes']['alt'] = "Search Button"; //add alt tag
-        unset($form['actions']['submit']['#value']); // Remove the value attribute from the input tag, since it is not valid when input type = image
+        //unset($form['actions']['submit']['#value']); // Remove the value attribute from the input tag, since it is not valid when input type = image
 
         // $form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/icon-search.png');
 
