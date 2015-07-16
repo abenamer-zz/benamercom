87235bd3e26fdf1feb1bb5ffdd9c880bc4f12602
Allan Benamer <allan@socialmediatoday.com>
2015-07-16 13:18:31 -0400

    Creating a dev environment specific settings file

--- settings.php
+++ settings.php
@@ -3,11 +3,12 @@ $cookie_domain = '.benamer.com';
 
 $update_free_access = FALSE;
 
+if (!empty($conf['load_development_overrides'])) {
+    require_once dirname(__FILE__) . '/settings.dev.php';
+}
+
 $local_settings = dirname(__FILE__) . '/settings.local.php';
 if (file_exists($local_settings)) {
     require_once($local_settings);
 }
 
-if (!empty($conf['load_development_overrides'])) {
-    require_once dirname(__FILE__) . '/settings.dev.php';
-}
