38ab72329274baea78547de193705668b1611492
Allan Benamer <allan@socialmediatoday.com>
2015-07-16 13:28:39 -0400

    Trying to read cookie_domain from an environment variable

--- settings.php
+++ settings.php
@@ -1,14 +1,16 @@
 <?php
-$cookie_domain = '.benamer.com';
 
 $update_free_access = FALSE;
 
-if (!empty($conf['load_development_overrides'])) {
-    require_once dirname(__FILE__) . '/settings.dev.php';
-}
+
 
 $local_settings = dirname(__FILE__) . '/settings.local.php';
 if (file_exists($local_settings)) {
     require_once($local_settings);
 }
 
+$cookie_domain = '.benamer.com';
+
+if (!empty($conf['cookie_domain'])) {
+    $cookie_domain=$conf['cookie_domain'];
+}
